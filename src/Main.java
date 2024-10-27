ee0481c30ae6b7bc8b7258f584b63eddbc33c51c
alexe <alexe9ko9@gmail.com>
2024-10-27 13:07:00 +0300

    Merge remote-tracking branch 'origin/hww' into hww

--- src/Main.java
+++ src/Main.java
@@ -64,12 +64,79 @@ public class Main {
                 System.out.println(" Значение переменной l с типом float равно "+l);
                 System.out.println(" Значение переменной z с типом double равно "+z);
                 float fl= 27.12f;
-                long lo= 987 678 965 549;
-                float fl1= 2.786f;
+        long lo= 987678965;
+        float fl1= 2.786f;
                 short sh=569;
                 short sh1=-159;
                 short sh2=27897;
                 byte by=67;
+                var lp=23;
+                var ac=27;
+                var ea=30;
+                var tot= 480;
+                var one1= tot/(by+lp+ac);
+                System.out.println(" На каждого ученика расчитано "+ one1+  " листов бумаги");
+
+var power = 8;
+var twenty = power*20;
+System.out.println(" За 20 минут машина произвела " + twenty + " штук бутылок");
+var day = 60*24* power;
+System.out.println(" За сутки машина произвела "+ day+ " штук бутылок");
+var threeDays = day*3;
+var month = day*30;
+System.out.println(" За три дня машина произвела "+threeDays+" штук бутылок");
+System.out.println(" За месяц машина произвела "+ month+ " штук бутылок");
+
+var pots= 120;
+var potsClass= pots/6;
+var whitePots= potsClass*2;
+var brownPots= potsClass*4;
+System.out.println("В школе, где "+potsClass+ " классов, нужно "+whitePots+ " банок белой краски и "+brownPots+ " банок коричневой краски");
+
+
+var banana= 80;
+var milk= 105;
+var iceCream= 50;
+var agg= 70;
+var totalWeight= banana*5+milk*2+iceCream*2+agg*4;
+float kg= totalWeight/1000f;
+System.out.println(" Общий вес продуктов "+totalWeight+" грамм или "+kg+ " килограмм");
+
+
+var minimal= 250;
+var maximal= 500;
+var result=7000;
+var low= result/minimal;
+var higth= result/maximal;
+System.out.println(" При потере веса 250 грамм в день потребуется "+low+" дней, а при петере веса по 500 грамм в день, потребуется "+higth+" дней");
+
+
+var salaryM=67760;
+var salaryD=83690;
+var salaryK=76230;
+var procentM=salaryM/10;
+var procentD=salaryD/10;
+var procentK=salaryK/10;
+var incomeM=salaryM*12;
+var incomeD=salaryD*12;
+var incomeK=salaryK*12;
+var upM=salaryM+procentM;
+var upD=salaryD+procentD;
+var upK=salaryK+procentK;
+var incomeUpM=upM*12;
+var incomeUpD=upD*12;
+var incomeUpK=upK*12;
+var upYearM=incomeUpM-incomeM;
+var upYearD=incomeUpD-incomeD;
+var upYearK=incomeUpK-incomeK;
+System.out.println(" Маша теперь получает "+upM+" рублей, годовой доход вырос на "+upYearM+" рублей");
+System.out.println(" Денис теперь получает "+upD+" рублей, годовой доход вырос на "+upYearD+" рублей");
+System.out.println(" Кристина теперь получает "+upK+" рублей, годовой доход вырос на "+upYearK+" рублей");
+
+
+
+
+
 
 
     }
